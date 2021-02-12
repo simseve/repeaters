a93c93327bf195d8ddb012d1627206189577af84
simseve <severini.simone@gmail.com>
2021-02-12 12:09:22 +0100

    publish

--- app.py
+++ app.py
@@ -29,27 +29,26 @@ def import_list():
 
     url = 'http://www.ik2ane.it/pontixls.xls'
     df = pd.read_excel(url)
-    # %%
     df = df.dropna(subset=['(F)req'])
-    # %%
     df = df.drop(['Agg.', '(K)m', 'Gradi', '(O)rdkey', 'JN45OL'], axis=1)
-    # %%
     df.dropna(subset=['(N)ome'], inplace=True)
 
     # Write to SQL
     df.to_sql('ponti', con=engine, if_exists='replace')
 
+    # Update the time of import as a global variable
     last_import = datetime.datetime.now()
 
 
 @app.route('/')
 def index():
-    # global last_import
-    # Connecting to a template (html file)
+
+    # DB operation: read all records
     conn = sqlite3.connect('app.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM ponti')
     ponti = cur.fetchall()
+    
     return render_template('index.html', ponti=ponti, update=last_import)
 
 if __name__ == '__main__':

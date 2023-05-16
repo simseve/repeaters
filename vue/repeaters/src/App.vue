a006452542274368d0415f200100a0be448712b1
simseve <severini.simone@gmail.com>
2023-05-16 09:36:38 +0200

    vue working

--- vue/repeaters/src/App.vue
+++ vue/repeaters/src/App.vue
@@ -1,45 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import TableRep from './components/TableRep.vue'
-</script>
-
 <template>
-
-  <main>
-    <div>
-    <table-rep>
-
-    </table-rep>
-  </div>
-  </main>
+  <section>
+    <header>
+      <h1>Ponti ripetitori</h1>
+    </header>
+    <TableRep />
+  </section>
 </template>
 
-<style scoped>
+<script>
+import TableRep from './components/TableRep.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TableRep
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Jost', sans-serif;
+}
+body {
+  margin: 0;
+}
 header {
-  line-height: 1.5;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 100%;
+  max-width: 80rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app h2 {
+  font-size: 4rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- vue/repeaters/src/components/TableRep.vue
+++ vue/repeaters/src/components/TableRep.vue
@@ -1,13 +1,13 @@
-<template>
+<!-- 643cd615b54b64e48a3fb8545de2d44c379d9798ba86e08c7b5559519dc050f3
+  https://repeaters.seve-ai.com/data
+ -->
+ <template>
   <div>
-    <input
-      v-model="search"
-      type="text"
-      class="form-control"
-      placeholder="Search..."
-    />
-
-    <table class="table">
+    <div class="search-bar">
+      <input v-model="search" type="text" placeholder="Search..."/>
+    </div>
+    <div class="table-container">
+    <table>
       <thead>
         <tr>
           <th v-for="(value, key) in items[0]" :key="key">
@@ -15,52 +15,122 @@
           </th>
         </tr>
       </thead>
-
       <tbody>
-        <tr v-for="item in filteredItems" :key="item.id">
-          <td v-for="value in item" :key="value">
+        <tr v-for="(item, index) in filteredItems" :key="index">
+          <td v-for="(value, key) in item" :key="key">
             {{ value }}
           </td>
         </tr>
       </tbody>
     </table>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { ref, onMounted, computed } from 'vue'
+import axios from 'axios'
 
 export default {
-  data() {
-    return {
-      items: [],
-      search: '',
-    };
-  },
+  setup() {
+    const items = ref([]);
+    const search = ref('');
 
-  computed: {
-    filteredItems() {
-      return this.items.filter((item) => {
-        return Object.values(item).some((value) =>
-          value.toString().toLowerCase().includes(this.search.toLowerCase())
-        );
-      });
-    },
-  },
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://repeaters.seve-ai.com/data', {
+          headers: {
+            'X-API-Key': '643cd615b54b64e48a3fb8545de2d44c379d9798ba86e08c7b5559519dc050f3'
+          }
+        })
+        items.value = response.data.data;
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
-  async created() {
-  try {
-    const response = await axios.get('https://repeaters.seve-ai.com/data', {
-      headers: {
-        'X-API-Key': '643cd615b54b64e48a3fb8545de2d44c379d9798ba86e08c7b5559519dc050f3',
-      },
+    const filteredItems = computed(() => {
+      if (search.value === '') {
+        return items.value;
+      }
+
+      return items.value.filter(item => 
+        Object.values(item).some(val => 
+          val?.toString().toLowerCase().includes(search.value.toLowerCase())
+        )
+      )
     });
-    this.items = response.data;
-  } catch (error) {
-    console.error(error);
+
+    onMounted(fetchData);
+
+    return { items, search, filteredItems };
   }
-},
-
-
-};
+}
 </script>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+}
+
+.search-bar {
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 30%;
+  padding: 10px;
+  background-color: #f9f9f9;
+  z-index: 100;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+}
+
+.table-container {
+  margin-top: 5px; /* adjust this value based on the height of your search bar */
+  height: 600px;
+  overflow-y: scroll;
+  padding: 20px;
+}
+
+input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+  transition: box-shadow 0.3s ease;
+}
+
+input:focus {
+  box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+}
+
+table {
+  width: 90%;
+  border-collapse: collapse;
+  margin-top: 20px;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+  padding: 12px 20px;
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #4CAF50;
+  color: white;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: #ddd;
+  transition: background-color 0.3s ease;
+}
+</style>
